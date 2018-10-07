535096a7da35af6e9d99aadc2a820b7fb27312da
KaiCheng123 <313721509@qq.com>
2018-10-07 22:59:22 +0800

    第二次修改

--- app/src/main/java/com/example/a31372/calorierecorder/CalorieActivity.java
+++ app/src/main/java/com/example/a31372/calorierecorder/CalorieActivity.java
@@ -6,23 +6,26 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class CalorieActivity extends Activity {
 
-    private TextView data1;
     private Button btn1;
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
+    private ListView listView;
+    private ArrayList<String> arrayList = new ArrayList<String>();
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calorielayout);
 
         btn1 = (Button) findViewById(R.id.btn1);
-        data1 = (TextView) findViewById(R.id.data1);
         sharedPreferences = getSharedPreferences("calorie_data_file",Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -41,25 +44,39 @@ public class CalorieActivity extends Activity {
             case 2:
                 if(resultCode == RESULT_OK){
                     String result = data.getStringExtra("calorie_data");
-                    editor.putString("Value",result);
-                    editor.commit();
+                    arrayList.add(result);
+                    saveData();
                 }
                 break;
                 default:
         }
     }
 
+    private void saveData() {
+        editor.putInt("ListNumber", arrayList.size());
+        for (int i = 0; i < arrayList.size(); i++){
+            editor.putString("item_"+i, arrayList.get(i));
+        }
+        editor.commit();
+    }
+
+
     @Override
     protected void onStart() {
         super.onStart();
-        String value = sharedPreferences.getString("Value","");
-        data1.setText(value);
+        int ListNumber = sharedPreferences.getInt("ListNumber",0);
+        arrayList.clear();
+        for (int i = 0; i < ListNumber; i++){
+            String ListItem = sharedPreferences.getString("item_"+i, null);
+            arrayList.add(ListItem);
+        }
+        listView = (ListView) findViewById(R.id.list_item);
+        listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,arrayList));
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        editor.putString("Value",data1.getText().toString().trim());
-        editor.commit();
+        saveData();
     }
 }
--- app/src/main/java/com/example/a31372/calorierecorder/MainActivity.java
+++ app/src/main/java/com/example/a31372/calorierecorder/MainActivity.java
@@ -1,19 +1,38 @@
 package com.example.a31372.calorierecorder;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.speech.RecognitionService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private EditText begin;
+    private EditText end;
+    private GraphView graph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button) findViewById(R.id.btn);
+        btn = (Button) findViewById(R.id.btn);
+        begin = (EditText) findViewById(R.id.text_begin);
+        end = (EditText) findViewById(R.id.text_end);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,5 +40,50 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        begin.setInputType(InputType.TYPE_NULL);    //不显示系统输入键盘
+        end.setInputType(InputType.TYPE_NULL);    //不显示系统输入键盘
+
+        begin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(1);
+            }
+        });
+
+        end.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(2);
+            }
+        });
+
+        graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+    }
+
+    /*
+    展示日期选择对话框
+     */
+    private void showDatePickerDialog(final int i) {
+        Calendar calendar = Calendar.getInstance();
+        new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                if(i == 1){
+                    begin.setText(year+"-"+(month+1)+"-"+dayOfMonth);
+                }
+                else if(i == 2){
+                    end.setText(year+"-"+(month+1)+"-"+dayOfMonth);
+                }
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 }
